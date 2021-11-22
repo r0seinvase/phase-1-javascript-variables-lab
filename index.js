12183215875a179183d70e951f6a2756151ddd83
Paola Moreno Reyes <nailamoreno99@gmail.com>
2021-11-22 16:49:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+// const fs = require('fs')
+// const path = require('path')
+
+// const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
+// describe('index.js', function () {
+//   describe('companyName', function () {
+//     it('is set as Scuber', function () {
+//       expect(companyName).to.equal('Scuber');
+//     });
+let companyName = 'Scuber'
+//     it('is defined as a const', function () {
+//       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
+//     });
+//   });
+
+//   describe('mostProfitableNeighborhood', function () {
+//     it('is declared as equal to Chelsea', function () {
+//       expect(mostProfitableNeighborhood).to.equal('Chelsea');
+//     });
+let mostProfitableNeighborhood = 'Chelsea'
+//     it('is defined using let', function () {
+//       expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
+//     });
+//   });
+
+//   describe('companyCeo', function () {
+//     it('is declared as equal to Susan Smith', function () {
+//       expect(companyCeo).to.equal('Susan Smith');
+//     });
+let companyCeo = 'Susan Smith'
+//     it('is defined using let', function () {
+//       expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
+//     });
+//   });
+// });
